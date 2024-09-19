4b51590d54775b1f53a7be9d70807febeb87e46c
Mr Aziz <mraziz1335@gmail.com>
2024-09-19 11:52:50 -0700

    pages linked

--- src/js/login.js
+++ src/js/login.js
@@ -1,34 +1,4 @@
-// function validateForm(event) {
-    // Get form elements
 
-    // var email = document.querySelector('input[type="email"]').value;
-    // var password = document.querySelector('input[type="password"]').value;
-
-    // Check if all fields are filled
-
-    // if (email === "" || password === "") {
-    //     alert("Please fill all the fields.");
-    //     return false;  // Don't proceed if validation fails
-    // }
-
-    // Check if the password meets the pattern requirement
-
-    // var passwordPattern = /[A-Z]+[0-9]+[a-z]+/; // At least one uppercase letter
-    // if (!passwordPattern.test(password)) {
-    //     alert("Password must contain at least one uppercase letter.");
-    //     return false;  // Don't proceed if password validation fails
-    // }
-
-    // If everything is correct, prevent the form from submitting
-
-    // event.preventDefault();
-
-    // Show the success message and redirect to the home page
-
-//     alert("Login successful!");
-//     window.location.href = "Home.html";  // Redirect to home page after success
-//     return true;
-// }
 
 function validateForm(event) {
     var email = document.querySelector('input[type="email"]').value;
@@ -39,11 +9,11 @@ function validateForm(event) {
         return false;
     }
 
-    var passwordPattern = /[A-Z]+/; // At least one uppercase letter
-    if (!passwordPattern.test(password)) {
-        alert("Password must contain at least one uppercase letter.");
-        return false;
-    }
+    // var passwordPattern = ''; // At least one uppercase letter
+    // if (!passwordPattern.test(password)) {
+    //     alert("Password must contain at least one uppercase letter.");
+    //     return false;
+    // }
 
     event.preventDefault();
     alert("Login successful!");
