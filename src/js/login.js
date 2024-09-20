88816d3eb8a9b595ac04ca722260298f09c6ecfc
Mr Aziz <mraziz1335@gmail.com>
2024-09-19 18:33:08 -0700

    password pattern change

--- src/js/login.js
+++ src/js/login.js
@@ -1,17 +1,9 @@
-
-
 function validateForm(event) {
     var email = document.querySelector('input[type="email"]').value;
     var password = document.querySelector('input[type="password"]').value;
 
     if (email === "" || password === "") {
         alert("Please fill all the fields.");
-        return false;
-    }
-
-    var passwordPattern = ''; // At least one uppercase letter
-    if (!passwordPattern.test(password)) {
-        alert("Password must contain at least one uppercase letter.");
         return false;
     }
 
@@ -22,13 +14,15 @@ function validateForm(event) {
 }
 
 function togglePasswordVisibility() {
-    var passwordField = document.querySelector('input[type="password"]');
+    var passwordField = document.getElementById('password');
     var toggleIcon = document.getElementById('toggle-icon');
+
+    // Toggle between showing and hiding password
     if (passwordField.type === "password") {
         passwordField.type = "text";
-        toggleIcon.src = "https://img.icons8.com/ios-filled/50/000000/visible.png"; // Icon for showing password
+        toggleIcon.src = "https://img.icons8.com/ios-filled/50/000000/visible.png"; // Show icon
     } else {
         passwordField.type = "password";
-        toggleIcon.src = "https://img.icons8.com/ios-filled/50/000000/invisible.png"; // Icon for hiding password
+        toggleIcon.src = "https://img.icons8.com/ios-filled/50/000000/invisible.png"; // Hide icon
     }
 }
